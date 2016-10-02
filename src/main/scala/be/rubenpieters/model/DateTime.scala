82660e13b1853a846843818f7de7df0142c4bd04
rubenpieters <ruben.pieters@devoteam.com>
2016-10-02 16:29:25 +0200

    impl timefields datetime3separator + basic case test

--- src/main/scala/be/rubenpieters/model/DateTime.scala
+++ src/main/scala/be/rubenpieters/model/DateTime.scala
@@ -1,6 +1,6 @@
 package be.rubenpieters.model
 
-import java.time.LocalDate
+import java.time.{LocalDate, LocalDateTime}
 import java.time.format.DateTimeFormatterBuilder
 
 import be.rubenpieters.util.Intersperse
@@ -11,16 +11,25 @@ import scala.language.implicitConversions
   * Created by ruben on 1/10/16.
   */
 
-case class DateTime3Separator()
+case class DateTime3Separator(dateOrder: DateOrder, timeOrder: TimeOrder, dateSeparator: String, dateTimeSeparator: String, timeSeparator: String)
+
+object DateTime3Separator {
+  def parseToLocalDateTime(dateTime3Separator: DateTime3Separator, text: String): ParserRepr => LocalDateTime = parserRepr => {
+    ParserReprOps.parse(
+      (Intersperse.intersperse(dateTime3Separator.dateOrder.order, Literal(dateTime3Separator.dateSeparator))
+        :+ Literal(dateTime3Separator.dateTimeSeparator))
+        ++ Intersperse.intersperse(dateTime3Separator.timeOrder.order, Literal(dateTime3Separator.timeSeparator))
+    )(parserRepr)
+      .parseToLocalDateTime(text)
+  }
+}
 
 case class DateTime2Separator()
 
 case class DateTime1Separator()
 
 
-case class Date1Separator(dateOrder: DateOrder, separator: String) {
-
-}
+case class Date1Separator(dateOrder: DateOrder, separator: String)
 
 object Date1Separator {
   def parseToLocalDate(date1Separator: Date1Separator, text: String): ParserRepr => LocalDate = parserRepr => {
@@ -36,6 +45,12 @@ sealed trait ParserRepr {
   def parseLiteral(literal: String): ParserRepr
 
   def parseToLocalDate(text: String): LocalDate
+
+  def parseHour(): ParserRepr
+  def parseMinute(): ParserRepr
+  def parseSecond(): ParserRepr
+
+  def parseToLocalDateTime(text: String): LocalDateTime
 }
 
 class JavaParserRepr extends ParserRepr {
@@ -59,6 +74,23 @@ class JavaParserRepr extends ParserRepr {
   }
 
   override def parseToLocalDate(text: String): LocalDate = LocalDate.parse(text, builder.toFormatter)
+
+  override def parseHour(): ParserRepr = {
+    builder.appendPattern("kk")
+    this
+  }
+
+  override def parseMinute(): ParserRepr = {
+    builder.appendPattern("mm")
+    this
+  }
+
+  override def parseSecond(): ParserRepr = {
+    builder.appendPattern("ss")
+    this
+  }
+
+  override def parseToLocalDateTime(text: String): LocalDateTime = LocalDateTime.parse(text, builder.toFormatter)
 }
 
 object ParserReprOps {
@@ -73,6 +105,9 @@ object ParserReprOps {
       case Year => parserRepr.parseYear()
       case Month => parserRepr.parseMonth()
       case Day => parserRepr.parseDay()
+      case Hour => parserRepr.parseHour()
+      case Minute => parserRepr.parseMinute()
+      case Second => parserRepr.parseSecond()
       case Literal(literal) => parserRepr.parseLiteral(literal)
     }
   }
@@ -81,18 +116,33 @@ object ParserReprOps {
 sealed trait ParserReprField
 case class Literal(literal: String) extends ParserReprField
 
-sealed trait DateField extends ParserReprField
+sealed trait DateTimeField extends ParserReprField
+
+sealed trait DateField extends DateTimeField
 case object Year extends DateField
 case object Month extends DateField
 case object Day extends DateField
+
+sealed trait TimeField extends DateTimeField
+case object Hour extends TimeField
+case object Minute extends TimeField
+case object Second extends TimeField
 
 case class DateOrder(order: List[DateField]) {
   require(order.distinct.size == order.size, "date order should not contain duplicates")
 }
 
+case class TimeOrder(order: List[TimeField]) {
+  require(order.distinct.size == order.size, "time order should not contain duplicates")
+}
+
 object DateOrder {
   implicit def dateTupleToOrder(tuple: (DateField, DateField, DateField)): DateOrder = {
     DateOrder(List(tuple._1, tuple._2, tuple._3))
+  }
+
+  implicit def dateTimeFieldTupleToOrder(tuple: (TimeField, TimeField, TimeField)): TimeOrder = {
+    TimeOrder(List(tuple._1, tuple._2, tuple._3))
   }
 }
 
--- src/test/scala/be/rubenpieters/model/DateTimeTest.scala
+++ src/test/scala/be/rubenpieters/model/DateTimeTest.scala
@@ -1,6 +1,6 @@
 package be.rubenpieters.model
 
-import java.time.LocalDate
+import java.time.{LocalDate, LocalDateTime}
 
 import org.scalatest.{FlatSpec, Matchers}
 import be.rubenpieters.model.DateOrder._
@@ -16,6 +16,14 @@ class DateTimeTest extends FlatSpec with Matchers {
 
     parsedDateYMD(new JavaParserRepr) shouldEqual LocalDate.of(2016, 10, 2)
     parsedDateMDY(new JavaParserRepr) shouldEqual LocalDate.of(2016, 10, 2)
+  }
+
+  "DateTime3Separator" should "be parsed in basic cases" in {
+    val parsedDateYMDHMS = DateTime3Separator.parseToLocalDateTime(DateTime3Separator((Year, Month, Day), (Hour, Minute, Second), "/", "|", ":"), "2016/10/02|16:25:55")
+    val parsedDateMDYSMH = DateTime3Separator.parseToLocalDateTime(DateTime3Separator((Month, Day, Year), (Second, Minute, Hour), "/", " ", "-"), "10/02/2016 55-25-16")
+
+    parsedDateYMDHMS(new JavaParserRepr) shouldEqual LocalDateTime.of(2016, 10, 2, 16, 25, 55)
+    parsedDateMDYSMH(new JavaParserRepr) shouldEqual LocalDateTime.of(2016, 10, 2, 16, 25, 55)
   }
 
 }
