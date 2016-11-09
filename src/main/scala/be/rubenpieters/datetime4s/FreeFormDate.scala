0f6b5e1e8a54b3ae75bfe0c721be9b75fd246e2e
ruben <rubnepieters@gmail.com>
2016-11-09 20:35:16 +0100

    prebuilt formatter

--- src/main/scala/be/rubenpieters/datetime4s/FreeFormDate.scala
+++ src/main/scala/be/rubenpieters/datetime4s/FreeFormDate.scala
@@ -2,19 +2,24 @@ package be.rubenpieters.datetime4s
 
 import java.time.{LocalDate, LocalDateTime}
 import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
+import java.util.Locale
 
 import be.rubenpieters.datetime4s.LiteralString
 
 /**
   * Created by ruben on 4/10/2016.
   */
-final case class FreeFormDate(fields: List[FormatterField])
+final case class FreeFormDate(fields: List[FormatterField], locale: Locale = Locale.getDefault(Locale.Category.FORMAT)) {
+  val preBuiltFormatter = {
+    val builder = new DateTimeFormatterBuilder()
+    fields.foreach(_.append(builder))
+    builder.toFormatter()
+  }
+}
 
 object FreeFormDate {
   def parse[T : DateTimeParseResult](freeFormDate: FreeFormDate, text: String) = {
-    val builder = new DateTimeFormatterBuilder()
-    freeFormDate.fields.foreach(_.append(builder))
-    DateTimeParseResult[T].parseWithDateTimeFormatter(text, builder.toFormatter)
+    DateTimeParseResult[T].parseWithDateTimeFormatter(text, freeFormDate.preBuiltFormatter)
   }
 }
 
